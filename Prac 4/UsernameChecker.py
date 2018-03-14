9d71c411545e19b931cf1855727130632e7bf291
Brody <brodyliston@gmail.com>
2018-03-15 11:12:35 +1300

    finished code and tested till worked

--- Prac 4/UsernameChecker.py
+++ Prac 4/UsernameChecker.py
@@ -2,3 +2,15 @@ usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole',
 'InterpreterInterface', 'StartServer', 'bob']
 
+
+
+def main():
+
+    userinput = str(input(" Enter Username "))
+
+    if userinput in usernames:
+        print("Access Granted!")
+    else:
+        print("Access Denied!")
+
+main()
